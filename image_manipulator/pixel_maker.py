d803478a553edb4492df81305b8703cb1f1dfe7e
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-08-21 21:03:14 +0200

    Delete unused test main function in pixel_maker.py

--- image_manipulator/pixel_maker.py
+++ image_manipulator/pixel_maker.py
@@ -6,49 +6,6 @@ from image_manipulator.resize import crop_image
 from image_manipulator.misc import RgbColor, Size
 
 
-def main():
-    input_image_name = input("Enter name of input image\n>")
-    image = Image.open(input_image_name)
-    pixels = image.load()
-    # pixels, image_size = load_image(input_image_name)
-    print(f"Size of input image: {image.size}")
-
-    pixel_size_input = input('Enter size of a pixel "width,height"\n>')
-    pixel_size_list = pixel_size_input.split(",")
-    pixel_size = Size(int(pixel_size_list[0]), int(pixel_size_list[1]))
-
-    pixel_count_width = math.floor(image.size[0] / pixel_size.width)
-    pixel_count_height = math.floor(image.size[1] / pixel_size.height)
-
-    num_pixels = pixel_size.width * pixel_size.height
-
-    for x_count in range(pixel_count_width):
-        for y_count in range(pixel_count_height):
-
-            image_color = RgbColor()
-
-            for x_pixel in range(pixel_size.width):
-                for y_pixel in range(pixel_size.height):
-                    pixel = pixels[
-                        x_pixel + x_count * pixel_size.width,
-                        y_pixel + y_count * pixel_size.height,
-                    ]
-                    image_color.add_rgb(pixel)
-
-            image_color.calculate_average(num_pixels)
-
-            for x_pixel in range(pixel_size.width):
-                for y_pixel in range(pixel_size.height):
-                    pixels[
-                        x_pixel + x_count * pixel_size.width,
-                        y_pixel + y_count * pixel_size.height,
-                    ] = image_color.average
-
-    output_path = "output/average_pixel.png"
-    image.save(f"{output_path}")
-    print(f"\nSee result in {output_path}")
-
-
 def set_pixels_size(pixels, pixel_size: Tuple[int, int], allow_crop: bool) -> Image:
     return pixelate(pixels, pixel_size[0], pixel_size[1], allow_crop)
 
@@ -102,10 +59,3 @@ def pixelate(image: Image, pixel_width: int, pixel_height: int, allow_crop: bool
             print("Pixelated area does not cover whole image, if this is not desired use flag --allow-crop to crop image")
 
     return image
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Keyboard cancelled")
