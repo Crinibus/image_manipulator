e23414f64430ab114339187c24eb94518908929f
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-08-21 21:59:14 +0200

    Use functions iterate_pixels and iterate_image_pixels instead of double loops

--- image_manipulator/average_color.py
+++ image_manipulator/average_color.py
@@ -2,7 +2,7 @@ from typing import Tuple
 from PIL import Image
 from PIL.PyAccess import PyAccess
 
-from image_manipulator.misc import RgbColor
+from image_manipulator.misc import RgbColor, iterate_pixels
 
 
 def get_average_color(pixels, image_size: Tuple[int, int]) -> Tuple[int, int, int]:
@@ -10,16 +10,15 @@ def get_average_color(pixels, image_size: Tuple[int, int]) -> Tuple[int, int, in
     pixel_count = 0
     image_color = RgbColor()
 
-    for x in range(image_width):
-        for y in range(image_height):
-            pixel = pixels[x, y]
+    for x, y in iterate_pixels(image_width, image_height):
+        pixel = pixels[x, y]
 
-            # skip pixels that are transparent
-            if pixel[3] == 0:
-                continue
+        # skip pixels that are transparent
+        if pixel[3] == 0:
+            continue
 
-            pixel_count += 1
-            image_color.add_rgb(pixel)
+        pixel_count += 1
+        image_color.add_rgb(pixel)
 
     image_color.calculate_average(pixel_count)
     return image_color.average_hex
--- image_manipulator/grid.py
+++ image_manipulator/grid.py
@@ -1,30 +1,29 @@
 from typing import Tuple
 
-from image_manipulator.misc import load_image
+from image_manipulator.misc import load_image, iterate_image_pixels
 from image_manipulator.resize import crop_image
 
 
 def create_grid_image(image_path, grid_size: Tuple[int, int], grid_rgb_color: Tuple[int, int, int], allow_crop: bool):
     grid_width, grid_height = grid_size
-    image, px = load_image(image_path)
+    image, pixels = load_image(image_path)
     image_width, image_height = image.size
 
     offset_x = image_width // grid_width
     offset_y = image_height // grid_height
 
-    for x_pixel in range(0, image_width):
-        for y_pixel in range(0, image_height):
-            # skip upper left corner pixel to avoid line at edge
-            if x_pixel == 0 and y_pixel == 0:
-                continue
-            # avoid lines at edges
-            if (x_pixel % offset_x == 0 and x_pixel == 0 and y_pixel % offset_y != 0) or (
-                y_pixel % offset_y == 0 and y_pixel == 0 and x_pixel % offset_x != 0
-            ):
-                continue
+    for x_pixel, y_pixel in iterate_image_pixels(image):
+        # skip upper left corner pixel to avoid line at edge
+        if x_pixel == 0 and y_pixel == 0:
+            continue
+        # avoid lines at edges
+        if (x_pixel % offset_x == 0 and x_pixel == 0 and y_pixel % offset_y != 0) or (
+            y_pixel % offset_y == 0 and y_pixel == 0 and x_pixel % offset_x != 0
+        ):
+            continue
 
-            if x_pixel % offset_x == 0 or y_pixel % offset_y == 0:
-                px[x_pixel - 1, y_pixel - 1] = grid_rgb_color
+        if x_pixel % offset_x == 0 or y_pixel % offset_y == 0:
+            pixels[x_pixel - 1, y_pixel - 1] = grid_rgb_color
 
     # minus one on each axis because don't want a line at the edges of the image
     nice_grid_image_size = (offset_x * grid_width - 1, offset_y * grid_height - 1)
