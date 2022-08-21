b923021a4fd4fdec142263aad461ec4ea6990cc3
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-08-21 21:52:12 +0200

    Add function return type hints and fix type hints on Image parameters

--- image_manipulator/resize.py
+++ image_manipulator/resize.py
@@ -4,17 +4,17 @@ from PIL import Image
 from image_manipulator.misc import load_image
 
 
-def resize_image_from_path(image_path: str, size: Tuple[int, int]):
-    img, px = load_image(image_path)
+def resize_image_from_path(image_path: str, size: Tuple[int, int]) -> Image.Image:
+    img, _ = load_image(image_path)
     resized_img = img.resize(size, Image.ANTIALIAS)
     return resized_img
 
 
-def resize_image(image: Image, size: Tuple[int, int]):
+def resize_image(image: Image.Image, size: Tuple[int, int]) -> Image.Image:
     resized_img = image.resize(size, Image.ANTIALIAS)
     return resized_img
 
 
-def crop_image(image: Image, upper_left_corner_pos: Tuple[int, int], new_size: Tuple[int, int]):
+def crop_image(image: Image.Image, upper_left_corner_pos: Tuple[int, int], new_size: Tuple[int, int]) -> Image.Image:
     cropped_image = image.crop((upper_left_corner_pos[0], upper_left_corner_pos[1], new_size[0], new_size[1]))
     return cropped_image
